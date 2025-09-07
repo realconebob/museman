791484e9dfddcc0666ebe4a0004ac60516f0fca8
Connor Walstrom <walst110@umn.edu>
2025-09-07 14:14:17 -0500

    Rewrite GenericFScanner to use dart:io package & async functions

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -18,122 +18,141 @@ enum Scanmode {
 
 /// OS agnostic interface for filesystem scanners
 abstract interface class FileScanner {
-  /// Set a new set of root paths for scanning
-  set setRoots(Set<Directory> roots);
-  /// Add a set of root paths to the existing list
-  set addRoots(Set<Directory> roots);
-  /// Delete a set of root paths from the existing list
-  set delRoots(Set<Directory> roots);
-  /// Clear all paths from the scan list
-  void clrRoots();
+  Future<void> setRoots(Set<Directory> roots);
+  Future<void> setPaths(Set<String> paths);
+
+  Future<void> addRoots(Set<Directory> roots);
+  Future<void> addPaths(Set<String> paths);
+
+  void delRoots(Set<Directory> roots);
+  void delPaths(Set<String> paths);
+
+  void clearStore();
 
   Set<Directory> get getRoots;
+  Set<String> get getPaths;
 
   /// Scan the list of root paths for files. Specify [roots] to scan a set of roots apart from the stored list.
   /// Specify [roots] and [set] to scan [roots] and do an operation on the stored list. Note: This does not modify and then scan,
   /// it scans the specified list of roots in [roots], and then modifies the stored list
-  Set<Directory>? scan({Set<Directory>? roots, Scanmode? set});
+  // Future<Set<FileSystemEntity>?> scan({Set<Directory>? roots, Scanmode? set});
 }
 
-/// Generic file scanner. Implements basic, system agnostic var checks to reduce code duplication
 class GenericFScanner implements FileScanner {
   final Set<Directory> _rootDirs;
 
-  GenericFScanner({Set<Directory>? rootDirs}): _rootDirs = HashSet<Directory>() {
-    if(rootDirs != null) {_addRoots(rootDirs);}
+  GenericFScanner({Set<Directory>? dirs}): _rootDirs = HashSet() {
+    if(dirs != null) {_rootDirs.addAll(dirs);}
   }
 
-  Future<Set<String>?> _validatePathSanity(Iterable<String> paths) async {
-    if(paths.isEmpty) throw InvalidArgumentException("Given empty paths list");
-
+  Future<Set<String>?> _validateRootPaths(Set<String> paths, {Set<Directory>? store}) async {
+    if(paths.isEmpty) {throw InvalidArgumentException("Given empty paths set");}
     Set<String> res = HashSet();
+    bool addToStore = (store != null);
+
     for(String cur in paths) {
-      if(!await Directory(cur).exists()) {res.add(cur);}
+      Directory temp = Directory(cur);
+      if(!await temp.exists()) { // If temp does not exist, it is not valid, so add it to the invalid set
+        res.add(cur);
+        continue;
+      }
+
+      if(addToStore) {store.add(temp);} // If temp does exist, it is valid; and if [valid] exists, add it to the user provided set
     }
 
     return (res.isEmpty) ? null : res;
   }
 
-  /// Validate an iterable list of paths, but throw an exception if any are insane
-  void _xValidatePathSanity(Iterable<String> paths) async {
-    Set<String>? insanePaths = await _validatePathSanity(paths);
-    if(insanePaths != null) {
-      throw InsanePathException("Got the following invalid paths: $insanePaths");
+  Future<void> _xValidateRootPaths(Set<String> paths, {Set<Directory>? store}) async {
+    Set<String>? invalidPaths = await _validateRootPaths(paths, store: store);
+    if(invalidPaths != null) throw InsanePathException("Insane paths: $invalidPaths");
+    return;
+  }
+
+  Future<Set<Directory>?> _validateRootDirs(Set<Directory> roots, {Set<Directory>? store}) async {
+    if(roots.isEmpty) throw InvalidArgumentException("Given empty roots set");
+    Set<Directory> res = HashSet();
+    bool addToStore = (store != null);
+
+    for(Directory cur in roots) {
+      if(!await cur.exists()) {
+        res.add(cur);
+        continue;
+      }
+
+      if(addToStore) {store.add(cur);}
     }
 
+    return (res.isEmpty) ? null : res;
+  }
+
+  Future<void> _xValidateRootDirs(Set<Directory> roots, {Set<Directory>? store}) async {
+    Set<Directory>? invalidDirs = await _validateRootDirs(roots, store: store);
+    if(invalidDirs != null) throw InsanePathException("Nonexistent paths: $invalidDirs");
+    return;
+  }
+
+  @override
+  Future<void> addPaths(Set<String> paths) async {
+    await _xValidateRootPaths(paths, store: _rootDirs);
     return;
   }
 
 
 
-  void _setRoots(Set<String> roots) {clrRoots(); _addRoots(roots);}
-  void _delRoots(Set<String> roots) {_rootDirs.removeAll(roots);}
-  @override void clrRoots() {_rootDirs.clear();}
-
-  void _addRoots(Set<String> roots) {
-    _xValidatePathSanity(roots);
-    _rootDirs.addAll(roots);
+  @override
+  Future<void> addRoots(Set<Directory> roots) async {
+    await _xValidateRootDirs(roots, store: _rootDirs);
+    return;
   }
-
-  Set<String> _getRoots() {return _roots;}
-
-
-
-  @override set setRoots(Set<String> roots) {_setRoots(roots);}
-  @override set addRoots(Set<String> roots) {_addRoots(roots);}
-  @override set delRoots(Set<String> roots) {_delRoots(roots);}
-  @override Set<String> get getRoots => _getRoots();
 
   @override
-  Set<String>? scan({Set<String>? roots, Scanmode? set}) {
-    throw UnimplementedError("GenericFScanner can not scan, as such an operation is OS specific");
+  void clearStore() {
+    _rootDirs.clear();
+    return;
   }
 
-  @override bool validatePathSanity(String path) {
-    return (_validatePathSanity([path]) == null) ? true : false;
+  @override
+  void delPaths(Set<String> paths) async {
+    Set<Directory> toDel = HashSet();
+    await _xValidateRootPaths(paths, store: toDel);
+    _rootDirs.removeAll(toDel);
   }
 
+  @override
+  void delRoots(Set<Directory> roots) async {
+    Set<Directory> toDel = HashSet();
+    await _xValidateRootDirs(roots, store: toDel);
+    _rootDirs.removeAll(toDel);
+  }
+
+  @override
+  Set<String> get getPaths {
+    Set<String> paths = HashSet();
+    for(Directory cur in _rootDirs) {
+      paths.add(cur.path);
+    }
+
+    return paths;
+  }
+
+  @override
+  Set<Directory> get getRoots {return _rootDirs;}
+
+  @override
+  Future<void> setPaths(Set<String> paths) async {
+    clearStore();
+    await addPaths(paths);
+    return;
+  }
+
+  @override
+  Future<void> setRoots(Set<Directory> roots) async {
+    clearStore();
+    await addRoots(roots);
+    return;
+  }
+
+
+  
 }
-
-
-
-class LinuxFScanner extends GenericFScanner implements FileScanner {
-  LinuxFScanner([super.roots]);
-
-  @override
-  Set<String>? _validatePathSanity(Iterable<String> paths) {
-      HashSet<String> res = HashSet<String>();
-
-      for(String cur in paths) {
-        if(cur is not sane) {res.add(cur);}
-      }
-
-    return (res.isEmpty) ? null : res;
-  }
-
-  @override
-  Set<String>? scan({Set<String>? roots, Scanmode? set}) {
-    // TODO: Implement this
-    return null;
-
-    // Set<String> res = HashSet<String>();
-    // Set<String> toScan = _roots;
-
-    // if(roots != null && set != null) {
-    //   switch(set) {
-    //     case Scanmode.scanAdd: _addRoots(roots); break;
-    //     case Scanmode.scanSet: _setRoots(roots); break;
-    //     case Scanmode.scanDel: _delRoots(roots); break;
-    //   }
-
-    //   toScan = roots;
-    // }
-
-    // // Do scan. Return null if error occurs
-    // return res;
-  }
-
-  // End of class
-}
-
-// TODO: Windows and MacOS scanners planned
